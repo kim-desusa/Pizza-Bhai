b403ce3526987249e20067c42909e087e50cb4e9
Ashikuzzaman Abir <ashikuzzamanabir@outlook.com>
2020-12-21 06:04:45 +0600

    puted some security

--- src/adminView.php
+++ src/adminView.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>admin view</title>
-</head>
-<body>
-  
-</body>
-</html>
+<?php
+session_start();
+if(isset($_SESSION['id'])==Null){
+  session_destroy();
+  header("Location:index.php");
+}
+?>
